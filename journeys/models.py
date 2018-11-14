8ef2af22edd20bb6f0e0a8d1cc062a07b4ede990
Ignisor <ignis2497@gmail.com>
2018-11-14 16:04:05 +0200

    added distance field to journey

--- journeys/models.py
+++ journeys/models.py
@@ -6,3 +6,4 @@ class Journey(models.Model):
     timestamp = models.DateTimeField(help_text='journey date and time')
     vehicle = models.ForeignKey('vehicles.Vehicle', related_name='journeys', on_delete=models.PROTECT)
     passengers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='journeys')
+    distance = models.PositiveIntegerField(help_text='distance of journey in kilometers')
